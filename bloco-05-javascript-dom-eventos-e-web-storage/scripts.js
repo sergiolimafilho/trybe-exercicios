5b5d9a1de72317934d221b831a0dd7b6f064787c
Sergio Lima <sfilhofut@gmail.com>
2022-07-02 11:47:19 -0300

    inicial

---
+++ bloco-05-javascript-dom-eventos-e-web-storage/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Velozes e Furiosos"
+document.getElementById("paragraph").innerText = "Melhor filme"
+document.getElementById("subtitle").innerText = "Operação Rio"
+document.getElementById("second-paragraph").innerText = "Brian O`conner, Dom, Mia, Letty"
