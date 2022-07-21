51fd5e5d50a85b457d053c851011b12e01602f54
Sergio Lima <sfilhofut@gmail.com>
2022-07-21 17:26:37 -0300

    HOF

---
+++ exercicios-8.1/script.js
@@ -0,0 +1,13 @@
+/* const greetingThalles = (how) => {
+    const who = 'Thalles';
+    if (how === 'giao') {
+        return `Opa cumpadi, cê ta bao ${who}`
+    }
+    return `Óla ${who}!`;
+}
+
+console.log(greetingThalles('giao')); */
+
+const giao = (nome) => `Òla ${nome}`;
+console.log(giao());
+
